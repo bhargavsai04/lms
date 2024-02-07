85c8577c24d9ed86ff16100283350263b92fad8b
bhargavsai04 <bhargavsai2099@gmail.com>
2024-02-07 21:07:09 +0530

    11th

--- migrations/20241022101436-add-noteId-in-chapter.js
+++ migrations/20241022101436-add-noteId-in-chapter.js
@@ -1,43 +1,34 @@
 'use strict';
 
-/** 
- * This module defines a Sequelize migration for adding the 'noteId' column to the 'Chapters' table.
- * @module Migration_AddNoteIdToChapters
- * @type {import('sequelize-cli').Migration} 
- */
-
+/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  /**
-   * Run the migration to add the 'noteId' column to the 'Chapters' table.
-   * @param {import('sequelize').QueryInterface} queryInterface - The Sequelize Query Interface
-   * @param {import('sequelize').Sequelize} Sequelize - The Sequelize library
-   * @returns {Promise<void>} - A Promise that resolves when the migration is complete
-   */
   async up(queryInterface, Sequelize) {
-    // Add the 'noteId' column to the 'Chapters' table
-    await queryInterface.addColumn('Chapters', 'noteId', {
+    await queryInterface.addColumn('Chapters', 'NoteId', {
       type: Sequelize.DataTypes.INTEGER
-    });
-
-    // Add foreign key constraint for the 'noteId' column referencing the 'id' column of the 'Notes' table
+    })
     await queryInterface.addConstraint('Chapters', {
-      fields: ['noteId'],
+      fields: ['NoteId'],
       type: 'foreign key',
       references: {
         table: 'Notes',
         field: 'id'
       }
-    });
+    })
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
   },
 
-  /**
-   * Reverse the migration to remove the 'noteId' column from the 'Chapters' table.
-   * @param {import('sequelize').QueryInterface} queryInterface - The Sequelize Query Interface
-   * @param {import('sequelize').Sequelize} Sequelize - The Sequelize library
-   * @returns {Promise<void>} - A Promise that resolves when the migration is complete
-   */
   async down(queryInterface, Sequelize) {
-    // Remove the 'noteId' column from the 'Chapters' table
-    await queryInterface.removeColumn('Chapters', 'noteId');
+    await queryInterface.removeColumn('Chapters','NoteId')
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
   }
 };
